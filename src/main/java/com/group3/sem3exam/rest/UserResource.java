1216d9a4bc9844ccea1263a1243b3a67c6b83229
Thomas-Rosenkrans-Vestergaard <Thomas-Rosenkrans-Vestergaard@users.noreply.github.com>
2018-10-10 13:51:49 +0200

    1. Fixed spelling mistake 2. Shortened declaration of ReceivedUser class 3. Made UserFacade instance static

--- src/main/java/com/group3/sem3exam/rest/UserResource.java
+++ src/main/java/com/group3/sem3exam/rest/UserResource.java
@@ -7,7 +7,6 @@ import com.group3.sem3exam.facades.UserFacade;
 
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
-import javax.print.attribute.standard.Media;
 import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
@@ -15,8 +14,9 @@ import javax.ws.rs.core.Response;
 @Path("users")
 public class UserResource
 {
-    private Gson gson = new GsonBuilder().setPrettyPrinting().create();
-    private static EntityManagerFactory emf  = Persistence.createEntityManagerFactory("rest-api-pu");
+    private        Gson                 gson       = new GsonBuilder().setPrettyPrinting().create();
+    private static EntityManagerFactory emf        = Persistence.createEntityManagerFactory("rest-api-pu");
+    private static UserFacade           userFacade = new UserFacade(emf);
 
     @GET
     @Produces(MediaType.TEXT_PLAIN)
@@ -28,38 +28,17 @@ public class UserResource
     @POST
     @Produces(MediaType.APPLICATION_JSON)
     @Consumes(MediaType.APPLICATION_JSON)
-    public Response create(String content) {
-        RecievedUser      u  = gson.fromJson(content, RecievedUser.class);
-        UserFacade uf = new UserFacade(emf);
-        User resultUser = uf.createUser( u.getName(), u.getEmail(), u.getPasswordHash() );
-        return Response.ok( resultUser ).build();
+    public Response create(String content)
+    {
+        ReceivedUser receivedUser = gson.fromJson(content, ReceivedUser.class);
+        User         createdUser  = userFacade.createUser(receivedUser.name, receivedUser.email, receivedUser.password);
+        return Response.ok(createdUser).build();
     }
 
-    private class RecievedUser {
-        private String username;
-        private String password;
-        private String email;
-
-        public RecievedUser(String username, String password, String email)
-        {
-            this.username = username;
-            this.password = password;
-            this.email = email;
-        }
-
-        public String getName()
-        {
-            return this.username;
-        }
-
-        public String getPasswordHash()
-        {
-            return this.password;
-        }
-
-        public String getEmail()
-        {
-            return this.email;
-        }
+    private class ReceivedUser
+    {
+        public String name;
+        public String email;
+        public String password;
     }
 }
