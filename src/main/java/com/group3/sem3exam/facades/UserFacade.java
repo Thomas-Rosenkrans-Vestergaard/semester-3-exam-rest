883c48df6e062b268e3a2b194aed518e73615885
Kristoffer Tolboll <Kristofferthetolboll@gmail.com>
2018-10-12 10:27:07 +0200

    added the authentcation endpoint without JWT token

--- src/main/java/com/group3/sem3exam/facades/UserFacade.java
+++ src/main/java/com/group3/sem3exam/facades/UserFacade.java
@@ -2,6 +2,8 @@ package com.group3.sem3exam.facades;
 
 import com.group3.sem3exam.data.entities.User;
 import com.group3.sem3exam.data.repositories.TransactionalUserRepository;
+import com.group3.sem3exam.rest.authentication.AuthenticationException;
+import com.group3.sem3exam.rest.authentication.UserAuthenticator;
 
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
@@ -10,15 +12,23 @@ import javax.persistence.Persistence;
 
 public class UserFacade
 {
-    private EntityManager em;
+    private EntityManagerFactory emf;
     public UserFacade(EntityManagerFactory emf) {
-        em = emf.createEntityManager();
+        this.emf = emf;
     }
 
     public User createUser(String name, String email, String password)
     {
-        TransactionalUserRepository tup = new TransactionalUserRepository(em);
+        TransactionalUserRepository tup = new TransactionalUserRepository(emf);
         return tup.createUser(name, email, password);
 
+    }
+
+    public User authenticate(String email, String password) throws AuthenticationException
+    {
+        TransactionalUserRepository tup = new TransactionalUserRepository(emf);
+        UserAuthenticator uf = new UserAuthenticator(tup);
+
+        return uf.authenticate(email, password);
     }
 }
--- src/main/java/com/group3/sem3exam/rest/UserResource.java
+++ src/main/java/com/group3/sem3exam/rest/UserResource.java
@@ -4,6 +4,8 @@ import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import com.group3.sem3exam.data.entities.User;
 import com.group3.sem3exam.facades.UserFacade;
+import com.group3.sem3exam.rest.authentication.AuthenticationException;
+import com.group3.sem3exam.rest.authentication.UserAuthenticator;
 
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
@@ -14,30 +16,41 @@ import javax.ws.rs.core.Response;
 @Path("users")
 public class UserResource
 {
-    private        Gson                 gson       = new GsonBuilder().setPrettyPrinting().create();
-    private static EntityManagerFactory emf        = Persistence.createEntityManagerFactory("rest-api-pu");
-    private static UserFacade           userFacade = new UserFacade(emf);
-
-    @GET
-    @Produces(MediaType.TEXT_PLAIN)
-    public String get() throws Exception
-    {
-        return "Hello World";
-    }
+    private        Gson                 gson              = new GsonBuilder().setPrettyPrinting().create();
+    private static EntityManagerFactory emf               = Persistence.createEntityManagerFactory("rest-api-pu");
+    private static UserFacade           userFacade        = new UserFacade(emf);
 
     @POST
     @Produces(MediaType.APPLICATION_JSON)
     @Consumes(MediaType.APPLICATION_JSON)
     public Response create(String content)
     {
-        ReceivedUser receivedUser = gson.fromJson(content, ReceivedUser.class);
-        User         createdUser  = userFacade.createUser(receivedUser.name, receivedUser.email, receivedUser.password);
+        ReceivedCreateUser receivedUser = gson.fromJson(content, ReceivedCreateUser.class);
+        User createdUser = userFacade
+                .createUser(receivedUser.name, receivedUser.email, receivedUser.password);
         return Response.ok(createdUser).build();
     }
 
-    private class ReceivedUser
+
+    @POST
+    @Produces(MediaType.APPLICATION_JSON)
+    @Consumes(MediaType.APPLICATION_JSON)
+    public Response authenticateUser(String content) throws AuthenticationException
+    {
+        ReceivedAuthenticateUser receivedUser = gson.fromJson(content, ReceivedAuthenticateUser.class);
+        userFacade.authenticate(receivedUser.email, receivedUser.password);
+        return Response.ok().build();
+    }
+
+    private class ReceivedCreateUser
     {
         public String name;
+        public String email;
+        public String password;
+    }
+
+    private class ReceivedAuthenticateUser
+    {
         public String email;
         public String password;
     }
