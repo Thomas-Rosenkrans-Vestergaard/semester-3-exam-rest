0df74a2b316caa19f90540b01323232cb3a7af5d
Kristoffer Tolboll <Kristofferthetolboll@gmail.com>
2018-10-24 12:16:21 +0200

    added more

--- src/main/java/com/group3/sem3exam/rest/ImageResource.java
+++ src/main/java/com/group3/sem3exam/rest/ImageResource.java
@@ -2,10 +2,13 @@ package com.group3.sem3exam.rest;
 
 import com.google.gson.Gson;
 import com.group3.sem3exam.data.entities.Image;
+import com.group3.sem3exam.data.repositories.JpaUserRepository;
+import com.group3.sem3exam.data.repositories.UserRepository;
 import com.group3.sem3exam.facades.DataUriEncoder;
 import com.group3.sem3exam.facades.ImageFacade;
-import com.group3.sem3exam.rest.authentication.Authenticator;
-import com.group3.sem3exam.rest.dto.CityDTO;
+import com.group3.sem3exam.rest.authentication.AuthenticationContext;
+import com.group3.sem3exam.rest.authentication.AuthenticationException;
+import com.group3.sem3exam.rest.authentication.TokenAuthenticator;
 import com.group3.sem3exam.rest.dto.ImageDTO;
 import com.group3.sem3exam.rest.exceptions.ImageNotFoundException;
 
@@ -15,27 +18,31 @@ import javax.ws.rs.core.Response;
 import java.io.IOException;
 import java.util.Base64;
 import java.util.List;
+import java.util.function.Supplier;
 import java.util.stream.Collectors;
 
 @Path("images")
 public class ImageResource
 {
 
-    private static Gson           gson          = SpecializedGson.create();
-    private static ImageFacade    imageFacade   = new ImageFacade(JpaConnection.create());
-    private static DataUriEncoder encoder       = new DataUriEncoder();
-    private static Authenticator  authenticator = new Authenticator();
+    private static Gson               gson          = SpecializedGson.create();
+    private static ImageFacade        imageFacade   = new ImageFacade(JpaConnection.create());
+    private static DataUriEncoder     encoder       = new DataUriEncoder();
+    private static TokenAuthenticator authenticator = new TokenAuthenticator(() -> new JpaUserRepository(JpaConnection.create()));
+
 
     @POST
     @Path("create")
     @Consumes(MediaType.APPLICATION_JSON)
-    public Response create(@HeaderParam("Authorization")String token, String content) throws IOException
+    public Response create(@HeaderParam("Authorization")String token, String content) throws IOException, AuthenticationException
     {
-
-        ReceivedCreateImage image = gson.fromJson(content, ReceivedCreateImage.class);
-        byte[]              data  = Base64.getDecoder().decode(image.data);
-        String              URI   = encoder.bytesToDataURI(data);
-        return Response.ok(URI).build();
+        AuthenticationContext  authenticationContext = authenticator.authenticate(token);
+        if(authenticationContext.getType().equals("USER")) {
+            ReceivedCreateImage image = gson.fromJson(content, ReceivedCreateImage.class);
+            byte[]              data  = Base64.getDecoder().decode(image.data);
+            String              URI   = encoder.bytesToDataURI(data);
+            return Response.ok(URI).build();
+        }
     }
 
 
@@ -51,6 +58,7 @@ public class ImageResource
     @Path("get/{user: [0-9]+}")
     public Response getByUser(@PathParam("user")int user, @HeaderParam("Authorization") String content) throws ImageNotFoundException
     {
+
     List<Image> images = imageFacade.getByUser(user);
     String json = gson.toJson(images.stream().map(ImageDTO::basic).collect(Collectors.toList()));
     return Response.ok(json).build();
