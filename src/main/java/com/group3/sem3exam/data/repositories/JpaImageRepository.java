809fe1dfd06e7de890ce421190970fac595b1425
Kristoffer Tolboll <Kristofferthetolboll@gmail.com>
2018-10-25 17:12:31 +0200

    added correct repo to image

--- src/main/java/com/group3/sem3exam/data/repositories/JpaImageRepository.java
+++ src/main/java/com/group3/sem3exam/data/repositories/JpaImageRepository.java
@@ -1,27 +1,34 @@
 package com.group3.sem3exam.data.repositories;
 
+import com.group3.sem3exam.data.entities.City;
 import com.group3.sem3exam.data.entities.Image;
 import com.group3.sem3exam.data.entities.Region;
 import com.group3.sem3exam.data.entities.User;
 import com.group3.sem3exam.data.repositories.transactions.AbstractTransactionalRepository;
+import com.group3.sem3exam.data.repositories.transactions.JpaTransaction;
 import com.group3.sem3exam.data.repositories.transactions.Transaction;
 
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.NoResultException;
 import java.util.List;
+import java.util.Map;
+import java.util.Set;
 
-public class JpaImageRepository extends TransactionalCrudRepository<Image, Integer> implements ImageRepository
+public class JpaImageRepository extends JpaCrudRepository<Image, Integer> implements ImageRepository
 {
-
-
+    /**
+     * Creates a new {@link JpaCityRepository}.
+     *
+     * @param entityManager The entity manager that operations are performed upon.
+     */
     public JpaImageRepository(EntityManager entityManager)
     {
         super(entityManager, Image.class);
     }
 
     /**
-     * Creates a new {@link TransactionalRegionRepository}.
+     * Creates a new {@link JpaCityRepository}.
      *
      * @param entityManagerFactory The entity manager factory from which the entity manager - that operations are
      *                             performed upon - is created.
@@ -32,38 +39,42 @@ public class JpaImageRepository extends TransactionalCrudRepository<Image, Integ
     }
 
     /**
-     * Creates a new {@link TransactionalCityRepository}.
+     * Creates a new {@link JpaCityRepository}.
      *
      * @param transaction The transaction from which the entity manager - that operations are performed upon - is
      *                    created.
      */
-    public JpaImageRepository(Transaction transaction)
+
+    public JpaImageRepository(JpaTransaction transaction)
     {
         super(transaction, Image.class);
     }
 
-
     @Override
     public Image create(String title, String uri, User user)
     {
-        Image image = new Image(title, uri, user);
-        getEntityManager().persist(image);
-        return image;
+        return null;
     }
 
     @Override
-    public List<Image> getByUser(Integer user){
-            return getEntityManager()
-                    .createQuery("SELECT i FROM Image i WHERE i.user.id = :user", Image.class)
-                    .setParameter("user", user)
-                    .getResultList();
-
-        }
-
+    public List<Image> getByUser(Integer user)
+    {
+        return null;
+    }
 
     @Override
     public List<Image> getByUserPaginated(Integer user, int pageSize, int pageNumber)
     {
-
+        return null;
     }
+
+
+    /**
+     * Returns the cities in the region with the provided id.
+     *
+     * @param region The id of the region to return the cities of.
+     * @return The cities in the region with the provided id.
+     */
+
+
 }
