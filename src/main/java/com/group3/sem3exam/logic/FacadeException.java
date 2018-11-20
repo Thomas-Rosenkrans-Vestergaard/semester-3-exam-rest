495c5b139fd6b72ee055461400ca3cf433abbeaa
Thomas-Rosenkrans-Vestergaard <Thomas-Rosenkrans-Vestergaard@users.noreply.github.com>
2018-11-20 14:14:24 +0100

    send user dto with authentication dto, send cause from FacadeExceptionMapper

--- src/main/java/com/group3/sem3exam/logic/FacadeException.java
+++ src/main/java/com/group3/sem3exam/logic/FacadeException.java
@@ -5,13 +5,12 @@ public class FacadeException extends Exception
 
     private final String    errorName;
     private final String    errorMessage;
-    private final Throwable cause;
 
     public FacadeException(String errorName, String errorMessage, Throwable cause)
     {
+        super(cause);
         this.errorName = errorName;
         this.errorMessage = errorMessage;
-        this.cause = cause;
     }
 
     public String getErrorName()
@@ -22,11 +21,5 @@ public class FacadeException extends Exception
     public String getErrorMessage()
     {
         return this.errorMessage;
-    }
-
-    @Override
-    public Throwable getCause()
-    {
-        return this.cause;
     }
 }
--- src/main/java/com/group3/sem3exam/rest/dto/AuthenticationDTO.java
+++ src/main/java/com/group3/sem3exam/rest/dto/AuthenticationDTO.java
@@ -1,17 +1,21 @@
 package com.group3.sem3exam.rest.dto;
 
+import com.group3.sem3exam.data.entities.User;
+
 public class AuthenticationDTO
 {
 
-    public final String token;
+    public final String  token;
+    public final UserDTO user;
 
-    public AuthenticationDTO(String token)
+    public AuthenticationDTO(String token, UserDTO userDTO)
     {
         this.token = token;
+        this.user = userDTO;
     }
 
-    public static AuthenticationDTO basic(String token)
+    public static AuthenticationDTO basic(String token, User user)
     {
-        return new AuthenticationDTO(token);
+        return new AuthenticationDTO(token, UserDTO.basic(user));
     }
 }
