2df78a06b2a1221aa14781caa5c7bcb90c654801
Thomas Vestergaard <Thomas-Rosenkrans-Vestergaard@users.noreply.github.com>
2018-11-22 19:54:35 +0100

    documented ValidationViolation

--- src/main/java/com/group3/sem3exam/logic/validation/ValidationViolation.java
+++ src/main/java/com/group3/sem3exam/logic/validation/ValidationViolation.java
@@ -3,15 +3,46 @@ package com.group3.sem3exam.logic.validation;
 import java.io.Serializable;
 import java.util.Map;
 
+/**
+ * Represents a violation of some validation in {@link ResourceValidator}.
+ */
 public class ValidationViolation
 {
 
-    private String              message;
-    private String              checkName;
-    private String              attribute;
-    private Object              invalidValue;
+    /**
+     * A user-oriented description of why the violation occurred.
+     */
+    private String message;
+
+    /**
+     * The name of the check that failed.
+     */
+    private String checkName;
+
+    /**
+     * The name of the attribute the violation occurred.
+     */
+    private String attribute;
+
+    /**
+     * The invalid value.
+     */
+    private Object invalidValue;
+
+    /**
+     * The variables associated with the check that failed.
+     */
     private Map<String, ? extends Serializable> variables;
 
+    /**
+     * Creates a new {@link ValidationViolation}.
+     *
+     * @param message      A user-oriented description of why the violation occurred.
+     * @param checkName    The name of the check that failed.
+     * @param attribute    The name of the attribute the violation occurred.
+     * @param invalidValue The invalid value.
+     * @param variables    The variables associated with the check that failed.
+     */
     public ValidationViolation(String message, String checkName, String attribute, Object invalidValue, Map<String, ? extends Serializable> variables)
     {
         this.message = message;
@@ -21,26 +52,51 @@ public class ValidationViolation
         this.variables = variables;
     }
 
+    /**
+     * Returns t user-oriented description of why the violation occurred.
+     *
+     * @return A user-oriented description of why the violation occurred.
+     */
     public String getMessage()
     {
         return this.message;
     }
 
+    /**
+     * Returns the name of the check that failed.
+     *
+     * @return The name of the check that failed.
+     */
     public String getCheckName()
     {
         return this.checkName;
     }
 
+    /**
+     * Returns the name of the attribute the violation occurred.
+     *
+     * @return The name of the attribute the violation occurred.
+     */
     public String getAttribute()
     {
         return this.attribute;
     }
 
+    /**
+     * Returns the invalid value.
+     *
+     * @return The invalid value.
+     */
     public Object getInvalidValue()
     {
         return this.invalidValue;
     }
 
+    /**
+     * Returns the variables associated with the check that failed.
+     *
+     * @return The variables associated with the check that failed.
+     */
     public Map<String, ? extends Serializable> getVariables()
     {
         return this.variables;
