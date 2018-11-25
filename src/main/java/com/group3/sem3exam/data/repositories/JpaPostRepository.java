47685dc8399267f6744673b0b0cb05b5f2cc4b7a
Thomas Vestergaard <Thomas-Rosenkrans-Vestergaard@users.noreply.github.com>
2018-11-25 19:55:31 +0100

    temporary fix of bug causing SQLGrammarException to be thrown from getTimelinePosts

--- src/main/java/com/group3/sem3exam/data/repositories/JpaPostRepository.java
+++ src/main/java/com/group3/sem3exam/data/repositories/JpaPostRepository.java
@@ -3,11 +3,13 @@ package com.group3.sem3exam.data.repositories;
 import com.group3.sem3exam.data.entities.Post;
 import com.group3.sem3exam.data.entities.User;
 import com.group3.sem3exam.data.repositories.transactions.JpaTransaction;
+import org.hibernate.exception.SQLGrammarException;
 
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Query;
 import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.List;
 
 public class JpaPostRepository extends JpaCrudRepository<Post, Integer> implements PostRepository
@@ -49,14 +51,18 @@ public class JpaPostRepository extends JpaCrudRepository<Post, Integer> implemen
     @Override
     public List<Post> getTimelinePosts(Integer userId, Integer pageSize, Integer cutoff)
     {
-        EntityManager em = getEntityManager();
-        Query query = em.createQuery("SELECT p FROM Post p WHERE p.id < :cutoff AND p.author IN " +
-                                     "(SELECT f.pk.friend FROM Friendship f WHERE f.pk.owner.id = :userId) " +
-                                     "ORDER BY p.id DESC", Post.class);
-        query.setParameter("userId", userId);
-        query.setParameter("cutoff", cutoff == null ? Integer.MAX_VALUE : cutoff);
-        query.setMaxResults(pageSize);
-        List<Post> posts = query.getResultList();
-        return posts;
+        try {
+            EntityManager em = getEntityManager();
+            Query query = em.createQuery("SELECT p FROM Post p WHERE p.id < :cutoff AND p.author IN " +
+                                         "(SELECT f.pk.friend FROM Friendship f WHERE f.pk.owner.id = :userId) " +
+                                         "ORDER BY p.id DESC", Post.class);
+            query.setParameter("userId", userId);
+            query.setParameter("cutoff", cutoff == null ? Integer.MAX_VALUE : cutoff);
+            query.setMaxResults(pageSize);
+            List<Post> posts = query.getResultList();
+            return posts;
+        } catch (Exception e) {
+            return new ArrayList<>(); // TODO: fix SQLGrammarException, maybe caused by not having friends
+        }
     }
 }
