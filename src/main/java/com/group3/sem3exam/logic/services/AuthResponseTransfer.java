fba761f70b6ae36e9fbc12245a296cf7bdae32de
Jonas <jonasgroenbek@gmail.com>
2018-11-30 12:26:08 +0100

    fixed syntex error

--- src/main/java/com/group3/sem3exam/logic/services/AuthResponseTransfer.java
+++ src/main/java/com/group3/sem3exam/logic/services/AuthResponseTransfer.java
@@ -5,15 +5,16 @@ import com.group3.sem3exam.data.services.AuthRequest;
 import com.group3.sem3exam.data.services.Permission;
 
 import java.util.List;
+import java.util.Set;
 
 public class AuthResponseTransfer
 {
     public final AuthRequestTransfer request;
     public final UserTransfer        user;
     public final String              token;
-    public final List<Permission>    permissions;
+    public final Set<Permission>     permissions;
 
-    public AuthResponseTransfer(AuthRequest request, User user, String token, List<Permission> permissions)
+    public AuthResponseTransfer(AuthRequest request, User user, String token, Set<Permission> permissions)
     {
         this.request = new AuthRequestTransfer(request, false, false);
         this.user = new UserTransfer(user);
