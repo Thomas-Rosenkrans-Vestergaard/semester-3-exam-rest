3941536052d46bda0c75de1f5ce46ea973a857e9
Thomas Vestergaard <Thomas-Rosenkrans-Vestergaard@users.noreply.github.com>
2018-12-01 18:05:57 +0100

    added more dev data

--- src/main/resources/dev.sql
+++ src/main/resources/dev.sql
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8df8d887578cd6a643095254705ba4f125ee662f8b06544c41dc0e3192c5073a
-size 1230
+oid sha256:222aa0ce5c7e4d7206f445586ccca4f3a0b22bb920cd31263ea57e6f1cd417a0
+size 145659
